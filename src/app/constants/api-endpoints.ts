123494f5067f5aceaf6a613b6180b21a9dd4b5ce
alphaTanmoy <dtanmoy169@gmail.com>
2025-07-12 10:11:37 +0530

    endpoints updated

--- src/app/constants/api-endpoints.ts
+++ src/app/constants/api-endpoints.ts
@@ -10,45 +10,71 @@ export const S3_URL = 'http://localhost:8088';
 export const SELLER_URL = 'http://localhost:8089';
 export const USER_URL = 'http://localhost:8091';
 
-// Authentication Endpoints
-export const AUTH_SEND_OTP = AUTH_URL + '/sent/otp';
-export const AUTH_SIGN_IN = AUTH_URL + '/signIn';
-
-// Admin Endpoints
+//=====================================ADMIN MICROSERVICE ENDPOINTS=====================================>
 export const ADMIN_SIGN_IN = ADMIN_URL + '/signIn';
 export const ADMIN_SEND_OTP = ADMIN_URL + '/sent/otp';
+//=====================================ADMIN MICROSERVICE ENDPOINTS=====================================>
 
-// User Endpoints
-export const USER_SIGN_IN = USER_URL + '/signIn';
-export const USER_SEND_OTP = USER_URL + '/sent/otp';
 
-// Seller Endpoints
-export const SELLER_SIGN_IN = SELLER_URL + '/signIn';
-export const SELLER_SEND_OTP = SELLER_URL + '/sent/otp';
 
-// Doctor Endpoints
-export const DOCTOR_SIGN_IN = DOC_URL + '/signIn';
-export const DOCTOR_SEND_OTP = DOC_URL + '/sent/otp';
+//=====================================AUTH MICROSERVICE ENDPOINTS=====================================>
+export const AUTH_SEND_OTP = AUTH_URL + '/sent/otp';
+export const AUTH_SIGN_IN = AUTH_URL + '/signIn';
+//=====================================AUTH MICROSERVICE ENDPOINTS=====================================>
 
-// Customer Care Endpoints
-export const CUSTOMER_CARE_SIGN_IN = CORE_URL + '/signIn';
-export const CUSTOMER_CARE_SEND_OTP = CORE_URL + '/sent/otp';
 
-// Raider Endpoints
-export const RAIDER_SIGN_IN = CORE_URL + '/signIn';
-export const RAIDER_SEND_OTP = CORE_URL + '/sent/otp';
 
-// Master Endpoints
-export const MASTER_SIGN_IN = ADMIN_URL + '/signIn';
-export const MASTER_SEND_OTP = ADMIN_URL + '/sent/otp';
-
-// Core Endpoints
+//=====================================CORE MICROSERVICE ENDPOINTS=====================================>
 export const NAVBAR_LIST = CORE_URL + '/navbar/get';
 export const NAVBAR_LIST_TO_DISPLAY = CORE_URL + '/navbar/getNavbarListToDisplay';
 export const NAVBAR_LIST_ADD = CORE_URL + '/navbar/add';
 export const NAVBAR_LIST_EDIT = CORE_URL + '/navbar/edit';
 export const NAVBAR_LIST_DELETE = CORE_URL + '/navbar/delete';
 export const IS_PARENT_MENU = CORE_URL + '/navbar/isAParentMenu';
+//=====================================CORE MICROSERVICE ENDPOINTS=====================================>
 
-// User Endpoints
+
+
+//=====================================DOC MICROSERVICE ENDPOINTS=====================================>
+export const DOCTOR_SIGN_IN = DOC_URL + '/signIn';
+export const DOCTOR_SEND_OTP = DOC_URL + '/sent/otp';
+//=====================================DOC MICROSERVICE ENDPOINTS=====================================>
+
+
+
+
+//=====================================KYC MICROSERVICE ENDPOINTS=====================================>
+//=====================================KYC MICROSERVICE ENDPOINTS=====================================>
+
+
+
+
+//=====================================MANAGEMENT MICROSERVICE ENDPOINTS=====================================>
+export const CUSTOMER_CARE_SIGN_IN = MANAGEMENT_URL + '/customercare/signIn';
+export const RAIDER_SIGN_IN = MANAGEMENT_URL + '/raider/signIn';
+export const SEND_OTP = MANAGEMENT_URL + '/sent/otp';
+//=====================================MANAGEMENT MICROSERVICE ENDPOINTS=====================================>
+
+
+//=====================================PAYMENT MICROSERVICE ENDPOINTS=====================================>
+//=====================================PAYMENT MICROSERVICE ENDPOINTS=====================================>
+
+
+
+//=====================================S3 MICROSERVICE ENDPOINTS=====================================>
+//=====================================S3 MICROSERVICE ENDPOINTS=====================================>
+
+
+
+//=====================================SELLER MICROSERVICE ENDPOINTS=====================================>
+export const SELLER_SIGN_IN = SELLER_URL + '/signIn';
+export const SELLER_SEND_OTP = SELLER_URL + '/sent/otp';
+//=====================================SELLER MICROSERVICE ENDPOINTS=====================================>
+
+
+
+//=====================================USER MICROSERVICE ENDPOINTS=====================================>
+export const USER_SIGN_IN = USER_URL + '/signIn';
+export const USER_SEND_OTP = USER_URL + '/sent/otp';
 export const GET_ALL_CUSTOMERS = USER_URL + '/customer/getAll';
+//=====================================USER MICROSERVICE ENDPOINTS=====================================>
